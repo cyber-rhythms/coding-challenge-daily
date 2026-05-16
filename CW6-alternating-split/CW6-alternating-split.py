838e7edcb929d5f9c30a242facd48642e7d7f5c6
cyber-rhythms-xps15 <36315073+cyber-rhythms@users.noreply.github.com>
2026-05-16 18:54:22 +0100

    added another codewars problems 16-05-26

--- CW6-alternating-split/CW6-alternating-split.py
+++ CW6-alternating-split/CW6-alternating-split.py
@@ -37,10 +37,35 @@ Strengths:
 - Edge case handling is fine. Variables are expressive. Intuitive zipping.
 
 Deficiencies:
-- 
+- line 7 - remember strings are immutable so don't need to worry about creating a copy using a slice. 
 
 Remarks:
-- A much cleverer solution that is also efficine is to slice
+- Looking at the exploitation of the step in slicing makes my code look -- clumsy!
 
 Solution:
+- For this problem, we can make use of the slicing with customised steps `text[start:stop:step]`, meaning we save on O(n) time and space of creating index arrays.
+- text[1::2]` grabs all the odd-indexed elements, and `text[::2]` the evens!
+- Also note that joining on 
 """
+
+def encrypt(text, n):
+    if not text or n <= 0:
+        return text
+    
+    for _ in range(n):
+        text = text[1::2] + text[::2]
+    return text
+
+def decrypt(ciph_text, n):
+    if not ciph_text or n <= 0:
+        return ciph_text
+    
+    mid = len(text) // 2
+    res = list(ciph_text)
+
+    for _ in range(n):
+        res[1::2] = text[::mid]
+        res[::2] = text[mid:]
+        text = "".join(res)
+
+    return text
